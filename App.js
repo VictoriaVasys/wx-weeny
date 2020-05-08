8d57a45ae7390af854d69e2a4d41326427a3274f
Victoria Vasys <victoria.vasys@gmail.com>
2020-05-08 00:58:07 -0600

    Basic weather fetch & dynamic colors/icons

--- App.js
+++ App.js
@@ -1,19 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { API_KEY } from './utils/WeatherAPIKey';
+
+import Weather from './components/Weather';
+
+export default class App extends React.Component {
+  state = {
+    isLoading: true,
+    temperature: 0,
+    weatherCondition: null,
+    error: null
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: 'Error Getting Weather Condtions'
+        });
+      }
+    );
+  }
+
+  fetchWeather(lat = 25, lon = 25) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=imperial`
+    )
+      .then(res => res.json())
+      .then(json => {
+          // console.log(json);
+          this.setState({
+            temperature: json.main.temp,
+            weatherCondition: json.weather[0].main,
+            isLoading: false
+          });
+      })
+  }
+
+  render() {
+    const { isLoading, temperature, weatherCondition } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoading ? <Text>Fetching The Weather</Text> : <Weather weather={weatherCondition} temperature={temperature} />}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#fff'
+  }
 });
---
+++ utils/WeatherAPIKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = '87766407a2a2ad42cfe53b0113298488';
