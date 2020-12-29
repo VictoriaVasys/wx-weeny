d1d9793c4945e8a2c2610b1a77834ecb03ed8759
Victoria Vasys <victoria.vasys@gmail.com>
2020-12-28 18:16:35 -0700

    update assets

---
+++ server/datasources/googleGeocoder.js
@@ -0,0 +1,35 @@
+const { OPEN_WEATHER_MAP_KEY } = require('../../utils/APIKeys');
+
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class OpenWeatherMapOneCallAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'https://api.openweathermap.org/';
+  }
+
+  willSendRequest(request) {
+    request.params.set('appid', OPEN_WEATHER_MAP_KEY);
+    request.params.set('exclude', 'minutely');
+    request.params.set('lat', 40.7128);
+    request.params.set('lon', -74.0060);
+    request.params.set('units', 'imperial');
+  }
+
+  currentWxReducer(currentWx) {
+    return {
+      current: {
+        description: currentWx.current.weather[0].description,
+        id: currentWx.current.weather[0].main,
+        temperature: currentWx.current.temp,
+      }
+    };
+  }
+
+  async getWeatherByLatLon({ lat, lon }) {
+    const currentWxResponse = await this.get(`data/2.5/onecall`);
+    return this.currentWxReducer(currentWxResponse);
+  }
+}
+
+module.exports = OpenWeatherMapOneCallAPI;
